05f7634f283f6a3e29717995c2a378678d90b383
leedthanh <leedthanh@gmail.com>
2023-08-15 22:16:50 -0500

    adding query tables and analysis

--- EmployeeSQL/sql-challenge.sql
+++ EmployeeSQL/sql-challenge.sql
@@ -1,36 +1,36 @@
 -- drop table if exists department;
 
 create table departments (
-	dept_no varchar(50) primary key,
-	dept_name varchar(50) not null
+	dept_no varchar(5) primary key,
+	dept_name varchar(20) not null
 );
 
 create table titles (
-	title_id varchar(50) primary key,
-	titles varchar(50) not null
+	title_id varchar(5) primary key,
+	titles varchar(20) not null
 );
 
 create table employees (
 	emp_no INTEGER not NULL primary key,
-	emp_title_id VARCHAR(50) not NULL, 
+	emp_title_id VARCHAR(5) not NULL, 
 	foreign key (emp_title_id) references titles(title_id),
-	birth_date VARCHAR(50) not NULL,
-	first_name VARCHAR(50) not NULL,
-	last_name VARCHAR(50) not NULL,
-	sex VARCHAR(50) not NULL,
-	hire_date VARCHAR(50) not NULL
+	birth_date VARCHAR(10) not NULL,
+	first_name VARCHAR(30) not NULL,
+	last_name VARCHAR(30) not NULL,
+	sex VARCHAR(5) not NULL,
+	hire_date VARCHAR(30) not NULL
 );
 
 create table dept_emp (
 	emp_no integer not null,
 	foreign key (emp_no) references employees (emp_no),
-	dept_no varchar(50) not null,
+	dept_no varchar(5) not null,
 	foreign key (dept_no) references departments (dept_no),
 	primary key (emp_no,dept_no)
 );
 
 create table dept_manager (
-	dept_no varchar(50) not null,
+	dept_no varchar(5) not null,
 	foreign key (dept_no) references departments (dept_no),
 	emp_no integer not null,
 	foreign key (emp_no) references employees (emp_no),
